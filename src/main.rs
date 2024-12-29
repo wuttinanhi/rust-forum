5202c98430beeec12a6944d1b4d5ab91c5acd4bc
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2024-12-30 03:20:54 +0700

    add cors middleware

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use actix_cors::Cors;
 use actix_session::config::PersistentSession;
 use actix_session::{storage::CookieSessionStore, SessionMiddleware};
 use actix_web::cookie::time::Duration;
+use actix_web::http;
 // use actix_web::middleware::NormalizePath;
 // use actix_web::middleware::TrailingSlash;
 use actix_web::{
@@ -33,8 +35,14 @@ async fn main() -> std::io::Result<()> {
         .parse()
         .unwrap_or(3000);
 
+    let cors_origins = std::env::var("APP_CORS_ORIGINS")
+        .unwrap_or("http://localhost:3000,http://127.0.0.1:3000".to_string());
+
     // --- database setup ---
     let db_pool = initialize_db_pool();
+
+    println!("listening at http://{}:{}", host, port);
+    println!("APP_CORS_ORIGINS: {:?}", &cors_origins);
 
     HttpServer::new(move || {
         // get mode from env
@@ -79,7 +87,18 @@ async fn main() -> std::io::Result<()> {
             .session_lifecycle(PersistentSession::default().session_ttl(Duration::days(7)))
             .build();
 
-        // --- routes ---
+        // -- setup CORS ---
+        let cors_middleware = Cors::default()
+            .allowed_origin(&cors_origins)
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![
+                http::header::CONTENT_TYPE,
+                http::header::AUTHORIZATION,
+                http::header::ACCEPT,
+            ])
+            .max_age(3600);
+
+        // --- setup routes ---
         let users_scope = web::scope("/users")
             .service(users_login_route)
             .service(users_login_post_route)
@@ -100,6 +119,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(db_ref.clone())
             .app_data(handlebars_ref.clone())
             // .wrap(NormalizePath::new(TrailingSlash::Always))
+            .wrap(cors_middleware)
             .wrap(cookie_session_middleware)
             .service(users_scope)
             .service(posts_scope)
