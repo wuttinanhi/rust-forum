508eb6f079209b3eb0b5d590e90ef423d5ccde61
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2025-01-02 02:39:23 +0700

    wip fix if profile picture too large response will hang

--- src/routes/error.rs
+++ src/routes/error.rs
@@ -1,23 +1,34 @@
+use std::borrow::BorrowMut;
+
 use crate::utils::flash::{set_flash_message, FLASH_ERROR};
+use actix_multipart::MultipartError;
 use actix_session::SessionExt;
 use actix_web::{
     dev::ServiceResponse,
     http::header::{LOCATION, REFERER},
     middleware::ErrorHandlerResponse,
-    HttpResponse, Result,
+    HttpMessage, HttpResponse, Result,
 };
 
 pub fn fallback_error_handler<B: actix_web::body::MessageBody + 'static>(
     res: ServiceResponse<B>,
 ) -> Result<ErrorHandlerResponse<B>, actix_web::Error> {
-    let req = res.request().clone();
+    let mut req = res.request().clone();
     let session = &req.get_session();
     let response_error = res.response().error();
 
-    // dbg!(&response_error);
+    dbg!(&response_error);
 
     if let Some(error) = response_error {
-        let error_message = error.to_string();
+        let error_message = match error.as_error::<MultipartError>() {
+            // actix_web::error::PayloadError::Overflow
+            Some(MultipartError::Payload(e)) => e.to_string(),
+            _ => error.to_string(),
+            // _ => error.to_string(),
+            // None => todo!(),
+        };
+
+        dbg!(&error_message);
 
         set_flash_message(session, FLASH_ERROR, &error_message)?;
 
@@ -28,12 +39,19 @@ pub fn fallback_error_handler<B: actix_web::body::MessageBody + 'static>(
             .and_then(|h| h.to_str().ok())
             .unwrap_or("/");
 
+        dbg!(referer);
+
         let mut response = HttpResponse::Found();
         response.append_header((LOCATION, referer));
+        let response_finish = response.finish();
+
+        // empty request
+        // req.headers_mut().clear();
+        req.borrow_mut().take_payload();
 
         return Ok(ErrorHandlerResponse::Response(ServiceResponse::new(
             req,
-            response.finish().map_into_right_body(),
+            response_finish.map_into_right_body(),
         )));
 
         // set_flash_message(&session, FLASH_ERROR, &error_message)?;
