5736ed3cbd62e14f15a2dc6d965a081d3f3bb8df
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2025-01-07 14:50:41 +0700

    devsetup improve

--- devsetup.sh
+++ devsetup.sh
@@ -1,2 +1,3 @@
 sudo apt install -y libpq-dev
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/diesel-rs/diesel/releases/latest/download/diesel_cli-installer.sh | sh
+cargo install cargo-watch
