9b1f2485ed110754f50c9b218b64876c17c4a935
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2025-08-28 10:09:03 +0700

    update docker stuff
    - increase user name length to 32

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,14 @@ COPY . /app
 
 RUN cargo build --release
 
-#FROM gcr.io/distroless/cc-debian12
+# FROM gcr.io/distroless/cc-debian12
 FROM debian:12-slim
+
 WORKDIR /app
-COPY --from=build-env /usr/lib/x86_64-linux-gnu /usr/lib/x86_64-linux-gnu
+
+# COPY --from=build-env /usr/lib/x86_64-linux-gnu /usr/lib/x86_64-linux-gnu
+
+COPY --from=build-env /usr/lib/x86_64-linux-gnu/lib* /usr/lib/x86_64-linux-gnu/
 COPY --from=build-env /app/target/release/rustforum /app/rustforum
 COPY --from=build-env /app/templates /app/templates
 
--- src/entities/user.rs
+++ src/entities/user.rs
@@ -14,7 +14,7 @@ use crate::models::User;
 
 use futures::future::{ready, Ready};
 
-static NAME_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[a-zA-Z0-9 ]{2,16}$").unwrap());
+static NAME_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[a-zA-Z0-9 ]{2,32}$").unwrap());
 
 pub const SESSION_KEY_USER: &str = "user";
 
@@ -35,7 +35,7 @@ pub struct UserLoginFormData {
 pub struct UserRegisterFormData {
     #[validate(regex(
         path = *NAME_REGEX,
-        message = "Name must contain only letters, numbers and spaces. length between 2 and 16."
+        message = "Name must contain only letters, numbers and spaces. length between 2 and 32."
     ))]
     pub name: String,
 
@@ -76,7 +76,7 @@ pub struct UserChangePasswordFormData {
 pub struct UserUpdateFormData {
     #[validate(regex(
         path = *NAME_REGEX,
-        message = "Name must contain only letters, numbers and spaces. length between 2 and 16."
+        message = "Name must contain only letters, numbers and spaces. length between 2 and 32."
     ))]
     pub new_name: String,
 }
